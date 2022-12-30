0418ce13c0f8037ade5d13334bdcfbff768648af
SriLaksmi <104779537+SriLakshmi9322@users.noreply.github.com>
2022-12-30 22:18:54 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 30 21:44:30 2022
+
+@author: HP
+"""
+
+#Functions
+def add1(a,b):
+  return a+b
+
+def add2(a,b,c):
+   tmp = a+b
+   return tmp+c
+
+#Defaulut values
+def add3(a,b,c=20,d=40):
+    tmp = a+b+c
+    return tmp+d
+
+print(add1(10,20))
+print(add3(10,20))
+
+#Add3 has 4 params
+print(add3(3,4))
+print(add3(3,4,10))
