0a589b201890a6b46f7cdedf73cb2a9545dec3ee
SriLaksmi <104779537+SriLakshmi9322@users.noreply.github.com>
2022-12-30 22:16:28 +0530

    Add files via upload

---
+++ Basic_containers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 30 21:36:33 2022
+
+@author: HP
+"""
+
+#understanding basic types
+#types are inferred by runtime automatically
+
+a = 10.1
+print(a)
+print(type(a))
+
+b = 13.6
+print(type(b))
+
+c = 'abc'
+print(type(c))
+
+e = False 
+print(e)
+print(type(e))
